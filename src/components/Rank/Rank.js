760724ffcd0058dbf6ac1841d209b08a5954a54c
Munir Muratović <58771762+MunirMuratovic@users.noreply.github.com>
2019-12-25 16:27:19 +0100

    update count and name

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,8 @@ const Rank = ({ name, entries }) => {
     return (
         <div className="ma4 mt0">
             <div className="white f3">
-                {`${name} , your current rank is...`}            </div>
+                {`${name} , your current entry count is...`}
+            </div>
             <div className='white f1 '>
                 {entries}
             </div>
